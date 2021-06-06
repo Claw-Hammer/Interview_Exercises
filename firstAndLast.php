f2c561130141f3c3c558cd015f23ce4b86a56570
ClawHammer <jvca2005@gmail.com>
2021-06-06 15:47:33 -0400

    First exercise done

---
+++ firstAndLast.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+
+Given a sorted array, A, with possibly duplicated elements,
+find the indices of the first and last occurrences of a target element, x. 
+Return -1 if the target is not found.
+
+Example:
+Input: A = [1,3,3,5,7,8,9,9,9,15], target = 9
+Output: [6,8]
+
+Input: A = [100, 150, 150, 153], target = 150
+Output: [1,2]
+
+Input: A = [1,2,3,4,5,6,10], target = 9
+Output: [-1, -1]
+
+
+*/
+
+$arr = array(1,2,3,4,5,6,10);
+$target = 9;
+$first = '';
+$last = '';
+
+if(!in_array($target, $arr)) {
+    
+    echo "[-1,-1]";
+
+} else {
+
+    for($i=0; $i < count($arr); $i++) {
+        
+        if($arr[$i] == $target) {
+            $first = $i;
+            
+            for($j=$i+1; $j < count($arr); $j++) {
+                
+                if($arr[$j] == $target) {
+                    $last = $j;
+                }
+            }
+            
+            break;
+        }
+    }
+    
+    echo "[$first, $last]";
+}
